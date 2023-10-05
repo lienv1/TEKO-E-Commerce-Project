f5e9c765f7550adf9f11f1404e5ab5200837e733
lienv1 <bobby_lien@hotmail.com>
2023-10-05 16:51:32 +0200

    setup dockerfile and init for api server

---
+++ APIServer/init.sh
@@ -0,0 +1,12 @@
+# Clean and build with Maven
+mvn clean package test
+
+# Build the Docker image
+docker build -t api-image .
+
+# Run the Docker container
+# Get the local IPv4 address using hostname
+ip=$(hostname -I | awk '{print $1}')
+
+# Run your Docker container with the obtained IP
+docker run --name api-server-container -e SPRING_DATASOURCE_URL=jdbc:mysql://$ip:3306/apiserver -p 8080:8080 api-image
