a54a5b7bc4ec1e35b5c1f98f1f9ea39e0f8a886c
lienv1 <bobby_lien@hotmail.com>
2023-10-06 14:33:29 +0200

    setup client

--- APIServer/init.ps1
+++ APIServer/init.ps1
@@ -7,4 +7,4 @@ docker build -t api-image .
 # Get the local IPv4 address
 $localIP = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress
 
-docker run --name api-server-container -e SPRING_DATASOURCE_URL=jdbc:mysql://"$localIP":3306/Apiserver -p 8080:8080 api-image
+docker run --name api-server-container -d -e SPRING_DATASOURCE_URL=jdbc:mysql://"$localIP":3306/Apiserver -p 8080:8080 api-image
