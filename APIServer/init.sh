42839422da6df98bd75159d23a2265a1b97a19eb
lienv1 <bobby_lien@hotmail.com>
2023-10-06 15:17:57 +0200

    adding keycloak properties

--- APIServer/init.sh
+++ APIServer/init.sh
@@ -9,4 +9,4 @@ docker build -t api-image .
 ip=$(hostname -I | awk '{print $1}')
 
 # Run your Docker container with the obtained IP
-docker run --name api-server-container -e SPRING_DATASOURCE_URL=jdbc:mysql://$ip:3306/Apiserver -p 8080:8080 api-image
+docker run --name api-server-container -e CUSTOM_PROPERTY_HOST="$localIP" -p 8080:8080 api-image
