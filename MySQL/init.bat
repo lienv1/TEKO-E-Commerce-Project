4e626f136d2fe1e2e8e8b1fec7d39194a2ec4251
lienv1 <bobby_lien@hotmail.com>
2023-10-06 10:54:43 +0200

    update mysql init for keycloak

--- MySQL/init.bat
+++ MySQL/init.bat
@@ -1,1 +1,11 @@
-docker run -d --name mysql-container -e MYSQL_ROOT_PASSWORD=my-secret-pw -e MYSQL_DATABASE=apiserver -v "%cd%/mysql-data:/var/lib/mysql" -p 3306:3306 mysql:latest
+@echo off
+call docker run --name mysql-container -d -e MYSQL_ROOT_PASSWORD=my-secret-pw -e MYSQL_DATABASE=apiserver -v "%cd%/mysql-data:/var/lib/mysql" -p 3306:3306 mysql:latest
+
+:check_container
+echo "Create keycloak database"
+call docker exec -it mysql-container /bin/sh -c "mysql -u root -pmy-secret-pw -e 'CREATE DATABASE IF NOT EXISTS Keycloak;'"
+if errorlevel 1 (
+    echo "Try again... Please wait..."
+    timeout /t 5 /nobreak >nul
+    goto check_container
+)
