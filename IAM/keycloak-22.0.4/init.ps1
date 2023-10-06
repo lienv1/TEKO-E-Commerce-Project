1b246ef2f4fa5cd78366b8e28a9520a4639eb0d0
lienv1 <bobby_lien@hotmail.com>
2023-10-06 21:33:45 +0200

    bug fix in keycloak

--- IAM/keycloak-22.0.4/init.ps1
+++ IAM/keycloak-22.0.4/init.ps1
@@ -7,4 +7,4 @@ docker build -t keycloak-image .
 # Get the local IPv4 address
 $localIP = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress
 
-docker run --name keycloak-container -d -p 8180:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD="my-secret-pw" keycloak-image start-dev --db=mysql --db-password="my-secret-pw" --db-url=jdbc:mysql://10.2.109.121:3306/Keycloak --db-username=root --spi-theme-welcome-theme=keycloak
+docker run --name keycloak-container -d -p 8180:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD="my-secret-pw" keycloak-image start-dev --db=mysql --db-password="my-secret-pw" --db-url=jdbc:mysql://"$localIP":3306/Keycloak --db-username=root --spi-theme-welcome-theme=keycloak
