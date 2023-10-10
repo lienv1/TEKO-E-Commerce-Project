a5014651608cc0edafe22266de85125bb7a84e8b
lienv1 <bobby_lien@hotmail.com>
2023-10-10 19:48:55 +0200

    update

--- APIServer/src/main/java/apiserver/apiserver/model/Product.java
+++ APIServer/src/main/java/apiserver/apiserver/model/Product.java
@@ -31,27 +31,146 @@ public class Product {
 	private int taxCode;
 	private Date lastModified;
 
-	// Constructors, getters, setters, and other methods
-
-	// Constructors
 	public Product() {
-		// Default constructor
 	}
 
-	public Product(String productName, String description, String brand, int weight, String category, String group,
-			int pack, String searchIndex, String gtinUnit, String gtinPack, double price, int stock, String origin,
-			boolean discount, int taxCode) {
+	public Long getProductId() {
+		return productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public int getPack() {
+		return pack;
+	}
+
+	public String getSearchIndex() {
+		return searchIndex;
+	}
+
+	public String getGtinUnit() {
+		return gtinUnit;
+	}
+
+	public String getGtinPack() {
+		return gtinPack;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public boolean isDiscount() {
+		return discount;
+	}
+
+	public int getTaxCode() {
+		return taxCode;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public void setProductName(String productName) {
 		this.productName = productName;
+	}
+
+	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public void setBrand(String brand) {
 		this.brand = brand;
+	}
+
+	public void setWeight(int weight) {
 		this.weight = weight;
+	}
+
+	public void setCategory(String category) {
 		this.category = category;
+	}
+
+	public void setGroup(String group) {
 		this.group = group;
+	}
+
+	public void setPack(int pack) {
 		this.pack = pack;
+	}
+
+	public void setSearchIndex(String searchIndex) {
 		this.searchIndex = searchIndex;
+	}
+
+	public void setGtinUnit(String gtinUnit) {
 		this.gtinUnit = gtinUnit;
+	}
+
+	public void setGtinPack(String gtinPack) {
 		this.gtinPack = gtinPack;
+	}
+
+	public void setPrice(double price) {
 		this.price = price;
+	}
+
+	public void setStock(int stock) {
 		this.stock = stock;
 	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public void setDiscount(boolean discount) {
+		this.discount = discount;
+	}
+
+	public void setTaxCode(int taxCode) {
+		this.taxCode = taxCode;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	
+	
+	
 }
---
+++ APIServer/src/main/java/apiserver/apiserver/repo/ProductRepo.java
@@ -0,0 +1,11 @@
+package apiserver.apiserver.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import apiserver.apiserver.model.Product;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Long>{
+
+}
