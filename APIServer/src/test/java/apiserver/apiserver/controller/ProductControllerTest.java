faee950e7e049a039a4b0c78cd673af4e44ef073
lienv1 <bobby_lien@hotmail.com>
2023-10-15 23:17:43 +0200

    update test for Exception

--- APIServer/src/test/java/apiserver/apiserver/controller/ProductControllerTest.java
+++ APIServer/src/test/java/apiserver/apiserver/controller/ProductControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,19 +24,19 @@ import apiserver.apiserver.service.ProductService;
 
 @WebMvcTest(ProductController.class)
 class ProductControllerTest {
-	
+
 	@MockBean
 	private ProductService productService;
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	private Product product;
 	private List<Product> products;
-	
+
 	@BeforeEach
 	void initProduct() {
 		product = new Product();
@@ -51,10 +52,10 @@ class ProductControllerTest {
 		product.setPrice(6.50);
 		product.setStock(120);
 		product.setOrigin("Thailand");
-		
+
 		products = new ArrayList<Product>();
 		products.add(product);
-		
+
 		when(productService.addProduct(any(Product.class))).thenReturn(product);
 		when(productService.getAllProducts()).thenReturn(products);
 	}
@@ -67,11 +68,21 @@ class ProductControllerTest {
 		assertNotNull(product);
 		assertNotNull(product.getProductId());
 	}
-	
+
 	@Test
 	void testGetProductById() throws ProductNotFoundException {
+		when(productService.getProductById(this.product.getProductId())).thenReturn(this.product);
 		Product product = productService.getProductById(14326l);
 		assertNotNull(product);
+	}
+
+	@Test
+	void testGetProductByIdFail() throws ProductNotFoundException {
+		when(productService.getProductById(this.product.getProductId())).thenThrow(new ProductNotFoundException("Product doesn't exist"));
+
+		ProductNotFoundException ex = assertThrows(ProductNotFoundException.class,
+				() -> productService.getProductById(14326l));
+		assertEquals("Product doesn't exist", ex.getMessage());
 	}
 
 	@Test
