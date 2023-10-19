16b18afd0e6f84dcfe10ec6d368e546f6b928c34
lienv1 <bobby_lien@hotmail.com>
2023-10-19 11:14:11 +0200

    update setup and methods in OrderControllerTest

--- APIServer/src/main/java/apiserver/apiserver/controller/OrderController.java
+++ APIServer/src/main/java/apiserver/apiserver/controller/OrderController.java
@@ -1,12 +1,23 @@
 package apiserver.apiserver.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import apiserver.apiserver.exception.OrderNotFoundException;
+import apiserver.apiserver.model.Order;
 import apiserver.apiserver.security.AuthorizationService;
 import apiserver.apiserver.service.OrderService;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 
 	@Autowired
@@ -20,6 +31,23 @@ public class OrderController {
 		this.authorizationService = authorizationService;
 	}
 	
-	
+	@PreAuthorize("hasAnyRole('ADMIN','CUSTOMER')")
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Order> getOrderById(@PathVariable("id") Long id, Principal principal) {
+		
+		boolean isAuthenticated = authorizationService.isAuthenticatedByPrincipal(principal, principal.getName());
+		
+		if (!isAuthenticated) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+		
+		try {
+			Order order = orderService.getOrderById(id);
+			return new ResponseEntity<Order>(order,HttpStatus.OK);
+		} catch (OrderNotFoundException e) {
+			return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
+		}
+		
+	}
 	
 }
--- APIServer/src/test/java/apiserver/apiserver/controller/OrderControllerTest.java
+++ APIServer/src/test/java/apiserver/apiserver/controller/OrderControllerTest.java
@@ -1,14 +1,29 @@
 package apiserver.apiserver.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import apiserver.apiserver.exception.OrderNotFoundException;
 import apiserver.apiserver.model.Order;
 import apiserver.apiserver.model.OrderDetail;
 import apiserver.apiserver.model.Product;
@@ -53,9 +68,26 @@ class OrderControllerTest {
 		order.setOrderId(100000l);
 	}
 	
+	//Annotation with Keycloak no longer works in Spring Boot 3 like @WithMockUser(username="admin", roles= "{CUSTOMER}")
+	//This is an alternate solution
+	void preAuthorization(boolean authorized) {
+	     // Mocking Authentication
+        Authentication auth = mock(Authentication.class);  
+        when(auth.isAuthenticated()).thenReturn(authorized);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+	}
+	
+	void isAuthenticatedByPrincipal(boolean authenticated) {
+		when(authorizationService.isAuthenticatedByPrincipal(any(Principal.class), anyString())).thenReturn(authenticated);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getOrderByIdTest() throws Exception {
+		isAuthenticatedByPrincipal(true);
+		
+		when(orderService.getOrderById(order.getOrderId())).thenReturn(order);
+		mockMvc.perform(get("/order/id/"+order.getOrderId()).with(csrf()))
+		.andExpect(status().isOk());
 	}
 
 }
