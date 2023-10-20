c7334da313b00b94b6b0b514c54f4c1014f3409b
lienv1 <bobby_lien@hotmail.com>
2023-10-20 21:18:40 +0200

    adding backup creator for IAM

---
+++ IAM/keycloak-22.0.4/createBackup.sh
@@ -0,0 +1,2 @@
+docker exec -it keycloak-container /bin/sh -c "/opt/keycloak/bin/kc.sh export --dir /opt/keycloak/bin/exportedRealm --db-url=jdbc:mysql://192.168.1.129/Keycloak --db-username root --db-password my-secret-pw --users same_file"
+docker cp keycloak-container:/opt/keycloak/bin/exportedRealm %cd%/backup/
