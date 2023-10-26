8e19fd063db55aa46f925b7421bcc56b1bdab8dc
lienv1 <bobby_lien@hotmail.com>
2023-10-26 20:01:23 +0200

    update setups for backend

--- APIServer/init.ps1
+++ APIServer/init.ps1
@@ -1,10 +1,13 @@
 # Clean and build with Maven
 mvn clean package test
 # Build the Docker image
+docker stop api-container
+docker rm api-container
+docker rmi api-image
 docker build -t api-image .
 # Run the Docker container
 
 # Get the local IPv4 address
 $localIP = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress
 
-docker run --name api-server-container -d -e HOSTNAME="$localIP" -p 8080:8080 api-image
+docker run --name api-container -d -e HOSTNAME="$localIP" -p 8080:8080 api-image
