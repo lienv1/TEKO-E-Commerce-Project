3bfdb22129899f3af2c64984fa00bac83bb5367e
lienv1 <bobby_lien@hotmail.com>
2023-11-07 18:57:14 +0100

    configure splitter

---
+++ APIServer/src/main/java/apiserver/apiserver/configuration/CustomStringToListConverter.java
@@ -0,0 +1,23 @@
+package apiserver.apiserver.configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomStringToListConverter implements Converter<String, List<String>> {
+    private final String delimiter;
+
+    public CustomStringToListConverter(@Value("${custom.request.param.delimiter:¿}") String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    @Override
+    public List<String> convert(String source) {
+        return Arrays.asList(source.split(Pattern.quote(delimiter)));
+    }
+}
