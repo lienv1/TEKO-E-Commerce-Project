8faaa11b7b476b2df4e0a7ee3be9a1bbba9debc9
lienv1 <bobby_lien@hotmail.com>
2023-11-16 10:59:04 +0100

    update environment

--- AngularShop/src/environments/environment.ts
+++ AngularShop/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  backendAPI: "http://10.2.109.118/api",
-  fileServerAPI:"http://10.2.109.118/files",
-  keycloakAPI:"http://10.2.109.118/auth",
+  backendAPI: "http://localhost/api",
+  fileServerAPI:"http://localhost/files",
+  keycloakAPI:"http://localhost/auth",
   keycloakRealm : "E-Commerce",
   keycloakClient : "E-Commerce-Client"
 };
