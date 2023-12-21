54c7ef1f889556ea9008a13af3b6ed6d6dae21c9
lienv1 <bobby_lien@hotmail.com>
2023-12-21 10:23:07 +0100

    bugfix

--- AngularShop/src/app/thank-you-page/thank-you-page.component.ts
+++ AngularShop/src/app/thank-you-page/thank-you-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-thank-you-page',
   templateUrl: './thank-you-page.component.html',
   styleUrls: ['./thank-you-page.component.scss']
 })
-export class ThankYouPageComponent {
+export class ThankYouPageComponent implements OnInit {
+  
+  @ViewChild('mainContainer') mainContainer !: ElementRef<HTMLDivElement>
+  
+  ngOnInit(): void {
+    if (this.mainContainer)
+      this.scroll(this.mainContainer.nativeElement);
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 
 }
