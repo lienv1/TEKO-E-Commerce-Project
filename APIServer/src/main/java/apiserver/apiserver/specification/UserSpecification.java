cc9dfe5c2127ce016d6ef5be631060e63d40fe89
lienv1 <bobby_lien@hotmail.com>
2023-12-21 17:25:13 +0100

    removed zombie code

--- APIServer/src/main/java/apiserver/apiserver/specification/UserSpecification.java
+++ APIServer/src/main/java/apiserver/apiserver/specification/UserSpecification.java
@@ -45,70 +45,7 @@ public class UserSpecification implements Specification<User> {
 	        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
 	    }
 
-//	 public static Specification<User> userIdContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty() || !isNumeric(keyword)) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("userId")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	
-//	 public static Specification<User> usernameContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty()) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("username")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//
-//	    public static Specification<User> erpIdContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty() || !isNumeric(keyword)) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("erpId")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	    
-//	    public static Specification<User> firstnameContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty()) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("firstname")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	    
-//	    
-//	    public static Specification<User> lastnameContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty()) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("lastname")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	    
-//	    public static Specification<User> emailContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty()) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	    
-//	    public static Specification<User> phoneContains(String keyword) {
-//	        return (root, query, criteriaBuilder) -> {
-//	            if (keyword == null || keyword.isEmpty()) {
-//	                return criteriaBuilder.conjunction();
-//	            }
-//	            return criteriaBuilder.like(criteriaBuilder.lower(root.get("phone")), "%" + keyword.toLowerCase() + "%");
-//	        };
-//	    }
-//	    
+    
 	    public static boolean isNumeric(String str) {
 	        if (str == null) {
 	            return false;
