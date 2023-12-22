5a6b2524ce24ebc9d141dc9181788042d40fbe01
lienv1 <bobby_lien@hotmail.com>
2023-12-22 18:14:54 +0100

    add health controller

---
+++ APIServer/src/main/java/apiserver/apiserver/controller/HealthController.java
@@ -0,0 +1,27 @@
+package apiserver.apiserver.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        // Add custom logic to check if the application is ready
+        // Return a 200 OK response when ready, or a different status code if not ready
+        if (isApplicationReady()) {
+            return ResponseEntity.ok("Application is healthy");
+        } else {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Application is not ready");
+        }
+    }
+
+    private boolean isApplicationReady() {
+        // Implement your custom logic here to check if the application is ready
+        // Return true when ready, false if not
+        return true;
+    }
+}
