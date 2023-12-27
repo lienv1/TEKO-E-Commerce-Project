cd4812a19f85076d1650c97d5452a5f334de812c
lienv1 <bobby_lien@hotmail.com>
2023-12-27 23:30:05 +0100

    update health check

--- APIServer/src/main/java/apiserver/apiserver/controller/HealthController.java
+++ APIServer/src/main/java/apiserver/apiserver/controller/HealthController.java
@@ -3,12 +3,14 @@ package apiserver.apiserver.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("health")
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/check")
     public ResponseEntity<String> health() {
         // Add custom logic to check if the application is ready
         // Return a 200 OK response when ready, or a different status code if not ready
