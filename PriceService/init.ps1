104bcf3a6071ebfd3f313e9e6adcd83741031de9
lienv1 <bobby_lien@hotmail.com>
2024-01-11 17:34:25 +0100

    update

---
+++ PriceService/init.ps1
@@ -0,0 +1,13 @@
+# Clean and build with Maven
+mvn clean package
+# Build the Docker image
+docker stop priceservice-container
+docker rm priceservice-container
+docker rmi priceservice-image
+docker build -t priceservice-image .
+# Run the Docker container
+
+# Get the local IPv4 address
+$localIP = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress
+
+docker run --name priceservice-container -d -e HOSTNAME="$localIP" -e SERVICE_HTTPS_ENABLED=false -p 8081:8081 priceservice-image
