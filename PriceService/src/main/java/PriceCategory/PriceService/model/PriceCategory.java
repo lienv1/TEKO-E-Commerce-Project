db48af35f4d8356380351b52009ab411e7d553cf
lienv1 <bobby_lien@hotmail.com>
2024-01-11 18:18:40 +0100

    update

--- PriceService/src/main/java/PriceCategory/PriceService/model/PriceCategory.java
+++ PriceService/src/main/java/PriceCategory/PriceService/model/PriceCategory.java
@@ -1,12 +1,15 @@
 package PriceCategory.PriceService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class PriceCategory {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	private String product;
