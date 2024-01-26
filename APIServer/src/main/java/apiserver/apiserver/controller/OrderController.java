1dc1e3236e20975a92b631217175bdda7b674ca9
lienv1 <bobby_lien@hotmail.com>
2024-01-26 16:30:46 +0100

    update

--- APIServer/src/main/java/apiserver/apiserver/controller/OrderController.java
+++ APIServer/src/main/java/apiserver/apiserver/controller/OrderController.java
@@ -107,6 +107,7 @@ public class OrderController {
 		
 		try {
 			Order addedOrder = orderService.addOrder(order);
+			System.out.println("Erp id is " + addedOrder.getUser().getErpId());
 			System.out.println("Sending email...");
 			boolean emailSuccess = emailService.sendingOrderConfirmation(addedOrder,language);
 			System.out.println("Email Success: " + emailSuccess);
