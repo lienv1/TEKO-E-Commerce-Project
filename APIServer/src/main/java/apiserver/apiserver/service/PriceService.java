306f7aa612316b47afd0c1595d7e67ab4208d220
lienv1 <bobby_lien@hotmail.com>
2024-02-15 16:58:53 +0100

    update

--- APIServer/src/main/java/apiserver/apiserver/service/PriceService.java
+++ APIServer/src/main/java/apiserver/apiserver/service/PriceService.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import apiserver.apiserver.dto.PriceCategoryDTO;
@@ -22,46 +23,50 @@ public class PriceService {
 	private String host;
 	private String presharedKey;
 
-	public PriceService(
-			@Value("${custom.property.host}") String host,
+	public PriceService(@Value("${custom.property.host}") String host,
 			@Value("${custom.property.service.https.enabled}") boolean httpsEnabled,
 			@Value("${custom.property.presharedkey}") String preSharedKey) {
 		this.host = (httpsEnabled == true ? "https://" : "http://") + host;
 		this.presharedKey = preSharedKey;
 	}
-	
+
 	public Double getPrice(Long erpId, Product product, int quantity) {
 		RestTemplate restTemplate = new RestTemplate();
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.set("Authorization", presharedKey);
-		String url = host + ":8083/pricecategory/quantity/"+quantity+"/customer/"+ erpId;
-		
-		ResponseEntity<Double> response = restTemplate.postForEntity(url, new HttpEntity<>(product, headers),
-				Double.class);
-		if (response.getStatusCode().is2xxSuccessful()) {
+		String url = host + ":8083/pricecategory/quantity/" + quantity + "/customer/" + erpId;
+
+		try {
+			ResponseEntity<Double> response = restTemplate.postForEntity(url, new HttpEntity<>(product, headers),
+					Double.class);
+			if (response.getStatusCode().is2xxSuccessful()) {
 //			String responseBody = response.getBody();
-			return response.getBody();
-		} else
+				return response.getBody();
+			} else
+				return null;
+		} catch (RestClientException e) {
 			return null;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-	
+
 	public List<PriceCategoryDTO> getPrices(Long erpId, List<ProductDTO> products) {
 		RestTemplate restTemplate = new RestTemplate();
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.set("Authorization", presharedKey);
-		String url = host + ":8083/pricecategory/customer/"+ erpId;
-		
-		ResponseEntity<List<PriceCategoryDTO>> response = restTemplate.exchange(
-			    url, 
-			    HttpMethod.POST, 
-			    new HttpEntity<>(products, headers), 
-			    new ParameterizedTypeReference<List<PriceCategoryDTO>>() {}
-			);
-		
+		String url = host + ":8083/pricecategory/customer/" + erpId;
+
+		ResponseEntity<List<PriceCategoryDTO>> response = restTemplate.exchange(url, HttpMethod.POST,
+				new HttpEntity<>(products, headers), new ParameterizedTypeReference<List<PriceCategoryDTO>>() {
+				});
+
 		if (response.getStatusCode().is2xxSuccessful()) {
 //			String responseBody = response.getBody();
 			return response.getBody();
